ceeaacede7283c9b4d121dd8d5e0eb734ab8b2d4
박한범 <hb1398@naver.com>
2019-07-03 17:58:36 +0900

    AAA

--- src/components/ImgBanner.vue
+++ src/components/ImgBanner.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <v-img :src="imgSrc"
-           aspect-ratio="1.7">
+           aspect-ratio="1.7"
+           min-height="700"
+           max-height="700"
+           >
       <v-layout align-center justify-center row fill-height>
         <v-flex text-xs-center>
           <span class="text-shadow display-2 font-weight-light">
