b3bdce58d296b4046d7a7897e0f12cd3d0384b11
김동욱 <gospel306@naver.com>
2019-07-10 10:08:52 +0900

    to use it later

--- src/components/Myfooter.vue
+++ src/components/Myfooter.vue
@@ -1,14 +1,6 @@
 <template>
   <v-footer height="auto" color="light-blue lighten-5">
     <v-layout justify-center row wrap>
-      <weather
-       api-key="f3878217fcb9bcfb41a13f5e6ec031c6"
-       title="Weather"
-       :latitude="latitude"
-       :longitude="longitude"
-       language="en"
-       units="uk">
-      </weather>
       <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
         &copy;2019 — Vue project<br/>
         <strong>Posted by : 공수래공수거 </strong><br/>
@@ -17,25 +9,10 @@
   </v-footer>
 </template>
 <script>
-  import VueWeatherWidget from 'vue-weather-widget'
-  import 'vue-weather-widget/dist/css/vue-weather-widget.css'
-
+  import weather from "./weather"
   export default{
-    data(){
-      return{
-        latitude: '',
-        longitude: ''
-      }
-    },
     components:{
-      'weather': VueWeatherWidget
-    },
-    mounted(){
-      var vm = this
-      navigator.geolocation.getCurrentPosition(function(pos){
-        vm.latitude = pos.coords.latitude.toString()
-        vm.longitude = pos.coords.longitude.toString()
-      })
+      weather
     }
   }
 </script>
---
+++ src/components/weather.vue
@@ -0,0 +1,33 @@
+<template>
+<weather
+       api-key="f3878217fcb9bcfb41a13f5e6ec031c6"
+       title="Weather"
+       :latitude="latitude"
+       :longitude="longitude"
+       language="en"
+       units="uk">
+      </weather>
+</template>
+<script>
+  import VueWeatherWidget from 'vue-weather-widget'
+  import 'vue-weather-widget/dist/css/vue-weather-widget.css'
+
+  export default{
+    data(){
+      return{
+        latitude: '',
+        longitude: ''
+      }
+    },
+    components:{
+      'weather': VueWeatherWidget
+    },
+    mounted(){
+      var vm = this
+      navigator.geolocation.getCurrentPosition(function(pos){
+        vm.latitude = pos.coords.latitude.toString()
+        vm.longitude = pos.coords.longitude.toString()
+      })
+    }
+  }
+</script>
