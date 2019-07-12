cec959416b2b114ef133e8331a8e419dae9f8a41
김동욱 <gospel306@naver.com>
2019-07-12 17:21:00 +0900

    added graph

--- src/components/RepositoryGraph.vue
+++ src/components/RepositoryGraph.vue
@@ -38,10 +38,7 @@ export default{
                 stroke:{
                     curve: 'smooth'
                 },
-                title:{
-                    text: 'Number of committed per month',
-                    align: 'left'
-                },
+                
                 grid:{
                     row:{
                         colors: ['#f3f3f3','transparent'],
@@ -72,40 +69,43 @@ export default{
         async setCommits(){
             const response = await Api.getCommits(6016)
             this.commits = response
-
+            console.log(commits)
             const commits = this.commits;
             const com1 = {};
             const com2 = {};
             const com3 = {};
             const com4 = {};
             const com5 = {};
-            const date = new Date();
-            date.setMonth(date.getMonth()-1);
-            var ddate = date.toISOString().substring(0,10);
             var num = commits.length -1
+            const date = new Date(commits[num].created_at);
+            var ddate = date.toISOString().substring(0,10);
+            
             var current = commits[num].created_at.substring(0,10)
+            const startdate = ddate.substring(5,10)
             var now = new Date();
             var diff = Math.abs(now.getTime() - date.getTime())
             diff = Math.ceil(diff/(1000*3600*24))
-            for(var i = 0;i <= diff;i++){
-                com1[ddate] = 0;
-                com2[ddate] = 0;
-                com3[ddate] = 0;
-                com4[ddate] = 0;
-                com5[ddate] = 0;
+            for(var i = 0;i < diff;i++){
+                var day = ddate.substring(5,10)
+                console.log(ddate)
+                com1[day] = 0;
+                com2[day] = 0;
+                com3[day] = 0;
+                com4[day] = 0;
+                com5[day] = 0;
                 while(ddate == current){
                     if(commits[num].action_name == "pushed to"){
                         var id = commits[num].author_id;
                         if(id == 336){
-                            com1[ddate] ++;
+                            com1[day] ++;
                         }else if(id == 281){
-                            com2[ddate] ++;
+                            com2[day] ++;
                         }else if(id == 345){
-                            com3[ddate] ++;
+                            com3[day] ++;
                         }else if(id == 367){
-                            com4[ddate] ++;
+                            com4[day] ++;
                         }else if(id == 291){
-                            com5[ddate] ++;
+                            com5[day] ++;
                         }
                     }
                     num--;
@@ -116,8 +116,12 @@ export default{
                 date.setDate(date.getDate()+1);
                 ddate = date.toISOString().substring(0,10);
             }
-            
+            const latest = current.substring(5,10)
             this.chartOptions = {
+                title:{
+                    text: 'Number of commits from '+startdate+' to '+latest,
+                    align: 'left'
+                },
                 xaxis:{
                     categories: Object.keys(com1)
                 }
