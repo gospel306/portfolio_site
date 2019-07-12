17b1a99c9c09b054ce3ae84e188cb39f0c6fa059
오명현 <loucks@naver.com>
2019-07-12 15:51:07 +0900

    add Photo

--- src/components/MasterPiece.vue
+++ src/components/MasterPiece.vue
@@ -18,13 +18,37 @@
           <v-hover class="cursor">
             <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`">
               <v-img
-                src="https://images.unsplash.com/photo-1481299490292-adef25f13c3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                :src="PhotoImage"
                 aspect-ratio="2.75"
               ></v-img>
               <v-card-text class="pt-4" style="position: relative;">
-                <v-btn absolute color="orange" class="white--text" fab large right top>
+
+                <v-dialog class="dialog" v-model="dialog" persistent max-width="450">
+                      <template v-slot:activator="{ on }">
+
+
+                <v-btn absolute color="orange" v-on="on" @click.stop="dialog" class="white--text" fab large right top>
                   <v-icon>fa-image</v-icon>
                 </v-btn>
+
+              </template>
+                    <v-card>
+                      <v-card-title class="headline">사진 선택</v-card-title>
+                      <v-card-text>랜덤 이미지 또는 선택하는 사진을 업로드 해보세요!</v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <span class="ThumbnailLink" style="display:none"></span>
+                        <input type="file" ref="file" class="imgur" accept="image/*" data-max-size="5000" style="display:none"/>
+                        <v-btn class="cancelBtn" color="green darken-1" flat @click="dialog = false">cancel</v-btn>
+                        <v-btn class="saveBtn" color="green darken-1" flat @click="changeImage" style="display:none">OK</v-btn>
+                        <v-btn class="selectBtn" color="green darken-1" flat  @click="$refs.file.click()">Select IMG</v-btn>
+                        <v-btn class="randomBtn"color="green darken-1" flat @click="randomImage">Random</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+
+
+
                 <div>
                   <h3 class="headline mb-2">사진 첨부</h3>
                   <div>
@@ -89,8 +113,89 @@
 </template>
 
 <script>
+
+import $ from 'jquery';
 export default {
-  name: "MasterPiece"
+  mounted(){
+    $('input[type=file]').on("change", function () {
+      var change = this;
+        var $files = $(this).get(0).files;
+        if ($files.length) {
+
+            // Reject big files
+            if ($files[0].size > $(this).data("max-size") * 1024) {
+                console.log("Please select a smaller file");
+                return false;
+            }
+
+            // Replace ctrlq with your own API key
+            var apiUrl = 'https://api.imgur.com/3/image';
+            var apiKey = '5029977fb779471';
+
+            var formData = new FormData();
+            formData.append("image", $files[0]);
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": apiUrl,
+                "method": "POST",
+                "datatype": "json",
+                "headers": {
+                    "Authorization": "Client-ID " + apiKey
+                },
+                "processData": false,
+                "contentType": false,
+                "data": formData,
+                "type": 'POST',
+                beforeSend: function (xhr) {
+                    console.log("Uploading");
+                },
+                success: function (res) {
+                    console.log(res.data.link);
+                    Swal.fire({
+                      type: 'success',
+                      title: 'Success!!!',
+                      text: '이미지 업로드 완료',
+                      footer: '<a href='+res.data.link+' target="_blank">'+res.data.link+'</a>'
+                    }).then((result) => {
+                      if (result.value) {
+                        $(".ThumbnailLink").html('<a href='+res.data.link+' target="_blank" class="imgLink">'+res.data.link+'</a>');
+                        $(".saveBtn").css("display","inline");
+                        $(".randomBtn").css("display","none");
+                        $(".cancelBtn").css("display","none");
+                      }
+                    });
+                },
+                error: function () {
+                    alert("Failed");
+                }
+            }
+            $.ajax(settings).done(function (response) {
+                console.log("Done");
+            });
+        }
+    });
+  },
+  name: "MasterPiece",
+  data(){
+    return{
+      dialog:false,
+      PhotoImage:"https://images.unsplash.com/photo-1481299490292-adef25f13c3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    }
+  },
+  methods:{
+    changeImage(){
+      var imgLink = $(".imgLink").html();
+      this.PhotoImage = imgLink;
+      this.dialog = false;
+    },
+    randomImage(){
+      this.PhotoImage = "https://source.unsplash.com/random/1600x900";
+      this.dialog = false;
+    }
+  }
+
 };
 </script>
 
