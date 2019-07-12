5634878e10d0ed016bbacaabd871a6895c9673bc
오명현 <loucks@naver.com>
2019-07-12 11:33:41 +0900

    imageBanner & headerText

--- src/components/PortfolioList.vue
+++ src/components/PortfolioList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout mt-5 wrap>
-	  
+
     <v-flex v-for="i in portfolios.length > limits ? limits : portfolios.length" xs12 sm6>
       <Portfolio class="ma-3"
               :date="portfolios[i - 1].created_at.toString()"
@@ -12,7 +12,7 @@
 
     <v-flex xs12 text-xs-center round my-5 v-if="loadMore">
       <v-btn color="info" dark v-on:click="loadMorePortfolios"><v-icon size="25" class="mr-2">fa-plus</v-icon> 더 보기</v-btn>
-	  
+
 	  <v-btn color="info" dark to="portfolioWriter"><v-icon size="25" class="mr-2">fa-plus</v-icon> 추가하기</v-btn>
     </v-flex>
 	        <v-toolbar-items class="hidden-xs-only">
@@ -52,7 +52,10 @@ export default {
 		},
 		moveWriter() {
 			location.href="/";
-    	}
+    },
+    loadMorePortfolios(){
+
+    },
 	},
 }
 </script>
